17b21beaa1c40e590ac096e1528edac5b6867027
Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>
2014-09-27 17:20:50 +0200

    Add missing #includes
    Some header files in stagefright try to use DISALLOW_EVIL_CONSTRUCTORS
    (and even DISALLOW_IMPLICIT_CONSTRUCTORS, which isn't defined anywhere
    in stagefright) without including the proper header.
    
    With clang, this results in a build failure - with gcc,
    it results in DISALLOW_EVIL_CONSTRUCTORS/DISALLOW_IMPLICIT_CONSTRUCTORS
    not doing what they were intended to do, instead appearing as a method
    in the DWARF debug sections of files using the headers.
    
    Change-Id: Ie4e99affe98ed8fb1bfcf5ec604c19dcd5311c62
    Signed-off-by: Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>

--- include/media/stagefright/SurfaceMediaSource.h
+++ include/media/stagefright/SurfaceMediaSource.h
@@ -25,6 +25,8 @@
 #include <media/stagefright/MediaSource.h>
 #include <media/stagefright/MediaBuffer.h>
 
+#include "foundation/ABase.h"
+
 namespace android {
 // ----------------------------------------------------------------------------
 
@@ -230,7 +232,7 @@ private:
     bool mBuffersReleased;
 
     // Avoid copying and equating and default constructor
-    DISALLOW_IMPLICIT_CONSTRUCTORS(SurfaceMediaSource);
+    DISALLOW_EVIL_CONSTRUCTORS(SurfaceMediaSource);
 };
 
 // ----------------------------------------------------------------------------
