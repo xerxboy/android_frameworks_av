892456bc38178b3c2d08d7f209e7b313a316f058
Satya Krishna Pindiproli <satyak@codeaurora.org>
2014-11-18 16:29:27 +0530

    audio_policy: Fix for audio routing to speaker though BT is connected
    - When voice dialer is active and play is issued from the connected
      BT headset, audio is heard on speaker.
    - Speaker is selected in getDeviceForStrategy() although BT is connected
      as the flag 'mA2dpSuspended' remains 'true'. The flag is not updated in
      checkA2dpSuspend() as force usage for communication
      and record remains FORCE_BT_SCO as voice dialer is active.
    - Modify the conditional check to set the flag to 'false'.
    
    Change ported from I86ebb66cd165e8009bfa6193d2ad9ab383996283
    
    CRs-Fixed: 750370
    Change-Id: Ie6fd9a552386465390b10168f878dfceb82aa86b

--- services/audiopolicy/AudioPolicyManager.cpp
+++ services/audiopolicy/AudioPolicyManager.cpp
@@ -5196,7 +5196,7 @@ void AudioPolicyManager::checkA2dpSuspend()
     if (mA2dpSuspended) {
         if ((!isScoConnected ||
              ((mForceUse[AUDIO_POLICY_FORCE_FOR_COMMUNICATION] != AUDIO_POLICY_FORCE_BT_SCO) &&
-              (mForceUse[AUDIO_POLICY_FORCE_FOR_RECORD] != AUDIO_POLICY_FORCE_BT_SCO))) &&
+              (mForceUse[AUDIO_POLICY_FORCE_FOR_RECORD] != AUDIO_POLICY_FORCE_BT_SCO))) ||
              ((mPhoneState != AUDIO_MODE_IN_CALL) &&
               (mPhoneState != AUDIO_MODE_RINGTONE))) {
 
@@ -5206,7 +5206,7 @@ void AudioPolicyManager::checkA2dpSuspend()
     } else {
         if ((isScoConnected &&
              ((mForceUse[AUDIO_POLICY_FORCE_FOR_COMMUNICATION] == AUDIO_POLICY_FORCE_BT_SCO) ||
-              (mForceUse[AUDIO_POLICY_FORCE_FOR_RECORD] == AUDIO_POLICY_FORCE_BT_SCO))) ||
+              (mForceUse[AUDIO_POLICY_FORCE_FOR_RECORD] == AUDIO_POLICY_FORCE_BT_SCO))) &&
              ((mPhoneState == AUDIO_MODE_IN_CALL) ||
               (mPhoneState == AUDIO_MODE_RINGTONE))) {
 
