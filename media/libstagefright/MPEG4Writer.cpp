0d7c5fba848e85525c750b9a6887226b8e982ec6
Kim Zhang <xiazhang@codeaurora.org>
2015-01-16 13:04:59 +0800

    libstagefright: fix output buffer is not released by Writer when stopping
      When video recording is stopped due to max file size/duration reached,
      there is a media server crash caused by one output is not released by
      MPEG4Writer. So release the buffer in these cases
    
    Change-Id: Ib7d15fcf21aea06e856e9adc7b0bcdd01a376328
    CRs-Fixed: 782013

--- media/libstagefright/MPEG4Writer.cpp
+++ media/libstagefright/MPEG4Writer.cpp
@@ -2266,10 +2266,14 @@ status_t MPEG4Writer::Track::threadEntry() {
 
         if (mOwner->exceedsFileSizeLimit()) {
             mOwner->notify(MEDIA_RECORDER_EVENT_INFO, MEDIA_RECORDER_INFO_MAX_FILESIZE_REACHED, 0);
+            copy->release();
+            copy = NULL;
             break;
         }
         if (mOwner->exceedsFileDurationLimit()) {
             mOwner->notify(MEDIA_RECORDER_EVENT_INFO, MEDIA_RECORDER_INFO_MAX_DURATION_REACHED, 0);
+            copy->release();
+            copy = NULL;
             break;
         }
 
@@ -2386,6 +2390,8 @@ status_t MPEG4Writer::Track::threadEntry() {
             copy->release();
             err = UNKNOWN_ERROR;
             mSource->notifyError(err);
+            copy->release();
+            copy = NULL;
             return err;
         }
 
