bc6348006639c2a002d0249b7ef3430cb40d0814
Steve Kondik <steve@cyngn.com>
2015-03-12 06:44:48 -0700

    Revert "libmediaplayerservice: Add support for PCM offloading"
     * Needs refactor
    
    This reverts commit 27dba52dd7555bb82128794239770a79d894be3c.
    
    Change-Id: Ib6802c5a220e840f1049bb837e2b2e13ec5419e7

--- media/libmediaplayerservice/nuplayer/NuPlayer.cpp
+++ media/libmediaplayerservice/nuplayer/NuPlayer.cpp
@@ -910,7 +910,7 @@ void NuPlayer::onMessageReceived(const sp<AMessage> &msg) {
                 ALOGV("media rendering started");
                 notifyListener(MEDIA_STARTED, 0, 0);
             } else if (what == Renderer::kWhatAudioOffloadTearDown) {
-                ALOGI("Tear down audio offload, fall back to s/w path if due to error.");
+                ALOGV("Tear down audio offload, fall back to s/w path if due to error.");
                 int64_t positionUs;
                 CHECK(msg->findInt64("positionUs", &positionUs));
                 int32_t reason;
@@ -934,8 +934,9 @@ void NuPlayer::onMessageReceived(const sp<AMessage> &msg) {
 
                 performSeek(positionUs, false /* needNotify */);
                 if (reason == Renderer::kDueToError) {
-                    if (ExtendedUtils::is24bitPCMOffloadEnabled()) {
+				    if(ExtendedUtils::is24bitPCMOffloadEnabled()) {
                         sp<MetaData> audioMeta = mSource->getFormatMeta(true /* audio */);
+<<<<<<< HEAD
                         if (ExtendedUtils::is24bitPCMOffloaded(audioMeta)) {
                               ALOGV("Override pcm format to 16 bits");
                               ExtendedUtils::setKeyPCMFormat(audioMeta, AUDIO_FORMAT_PCM_16_BIT);
@@ -943,6 +944,14 @@ void NuPlayer::onMessageReceived(const sp<AMessage> &msg) {
                               mSource->start();
                         }
                     }
+=======
+                        if(ExtendedUtils::is24bitPCMOffloaded(audioMeta)) {
+					        ALOGV("Override pcm format to 16 bits");
+					        ExtendedUtils::setKeyPCMFormat(audioMeta, AUDIO_FORMAT_PCM_16_BIT );
+						    mSource->start();
+					    }
+				    }
+>>>>>>> 0f16f3e... Revert "libmediaplayerservice: Add support for PCM offloading"
                     mRenderer->signalDisableOffloadAudio();
                     mOffloadAudio = false;
                     mOffloadDecodedPCM = false;
@@ -1066,8 +1075,26 @@ void NuPlayer::onStart() {
             instantiateDecoder(true, &mAudioDecoder);
         }
     }
+<<<<<<< HEAD
 
     mSource->start();
+=======
+    bool overrideSourceStart = false;
+    if (ExtendedUtils::is24bitPCMOffloadEnabled()) {
+	//if 24 bit offloading is enabled and if its such a use
+	//case do not call start since this will be called
+	//after openAudioSink
+	     sp<MetaData> audioMeta = mSource->getFormatMeta(true /* audio */);
+		 overrideSourceStart = ExtendedUtils::is24bitPCMOffloaded(audioMeta);
+	}
+
+    if (overrideSourceStart) {
+	    ALOGV("%s: Do not start source, wait till openAudioSink");
+	} else {
+    mSource->start();
+	}
+
+>>>>>>> 0f16f3e... Revert "libmediaplayerservice: Add support for PCM offloading"
     uint32_t flags = 0;
 
     if (mSource->isRealTime()) {
@@ -1086,7 +1113,7 @@ void NuPlayer::onStart() {
     mOffloadAudio = canOffloadStream(audioMeta, (videoFormat != NULL), vMeta,
                          mIsStreaming /* is_streaming */, streamType);
      //For offloading decoded content
-    if (!mOffloadAudio && (audioMeta != NULL)) {
+     if (!mOffloadAudio && (audioMeta != NULL)) {
         sp<MetaData> audioPCMMeta =
                 ExtendedUtils::createPCMMetaFromSource(audioMeta);
 
@@ -1096,7 +1123,7 @@ void NuPlayer::onStart() {
         }
         mOffloadAudio =
                 ((mime && !ExtendedUtils::pcmOffloadException(mime)) &&
-                canOffloadStream(audioPCMMeta, (videoFormat != NULL), vMeta,
+                canOffloadStream(audioMeta, (videoFormat != NULL), vMeta,
                         mIsStreaming /* is_streaming */, streamType));
         mOffloadDecodedPCM = mOffloadAudio;
         ALOGI("Could not offload audio decode, pcm offload decided :%d",
@@ -1235,15 +1262,6 @@ void NuPlayer::tryOpenAudioSinkForOffload(const sp<AMessage> &format, bool hasVi
     // Note: This is called early in NuPlayer to determine whether offloading
     // is possible; otherwise the decoders call the renderer openAudioSink directly.
 
-    //update bit width before opening audio sink
-    if (ExtendedUtils::is24bitPCMOffloadEnabled()) {
-        sp<MetaData> audioMeta = mSource->getFormatMeta(true /* audio */);
-        if (ExtendedUtils::is24bitPCMOffloaded(audioMeta)) {
-            ALOGV("overriding format with 24 bits");
-            format->setInt32("sbit", 24);
-        }
-    }
-
     status_t err = mRenderer->openAudioSink(
             format, true /* offloadOnly */, hasVideo, AUDIO_OUTPUT_FLAG_NONE, mIsStreaming, &mOffloadAudio);
     if (err != OK) {
@@ -1306,6 +1324,7 @@ status_t NuPlayer::instantiateDecoder(bool audio, sp<DecoderBase> *decoder) {
         sp<MetaData> audioMeta = mSource->getFormatMeta(true /* audio */);
 
         if (mOffloadAudio && !mOffloadDecodedPCM) {
+<<<<<<< HEAD
             if (ExtendedUtils::is24bitPCMOffloadEnabled()) {
                 sp<MetaData> audioMeta = mSource->getFormatMeta(true /* audio */);
                 if (ExtendedUtils::is24bitPCMOffloaded(audioMeta)) {
@@ -1326,6 +1345,22 @@ status_t NuPlayer::instantiateDecoder(bool audio, sp<DecoderBase> *decoder) {
                     mSource->start();
                 }
             }
+=======
+             if(ExtendedUtils::is24bitPCMOffloadEnabled() &&
+			         ExtendedUtils::is24bitPCMOffloaded(audioMeta)) {
+                         //if offloaded, configure source for 24 bit
+                         ExtendedUtils::setKeyPCMFormat(audioMeta,AUDIO_FORMAT_PCM_8_24_BIT);
+					     mSource->start();
+			 }
+            *decoder = new DecoderPassThrough(notify, mSource, mRenderer);
+        } else {
+             if(ExtendedUtils::is24bitPCMOffloadEnabled() &&
+			         ExtendedUtils::is24bitPCMOffloaded(audioMeta)) {
+                         //if NOT offloaded, configure source for 16 bit
+                         ExtendedUtils::setKeyPCMFormat(audioMeta,AUDIO_FORMAT_PCM_16_BIT);
+					     mSource->start();
+			 }
+>>>>>>> 0f16f3e... Revert "libmediaplayerservice: Add support for PCM offloading"
             *decoder = new Decoder(notify, mSource, mRenderer);
         }
     } else {
